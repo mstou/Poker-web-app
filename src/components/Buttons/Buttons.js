6b5148b65be9fbf57862adac7f5e46fd39f5a4d6
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-07 01:32:54 +0300

    Remove unnecessary 'Change cards' button

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Buttons = ({winner,Ready,ChangeCards}) => (
   <div>
-    {(!winner) ? <button onClick={Ready}>Ready!</button> : undefined }
+    {(!winner) ? <button className="ReadyButton" onClick={Ready}>Ready!</button> : undefined }
     <p>{(winner) ? winner + " is the Winner!" : undefined }</p>
-    {(!winner) ? <button onClick={ChangeCards}> Change Cards!</button> : undefined }
+    {/*(!winner) ? <button onClick={ChangeCards}> Change Cards!</button> : undefined */}
   </div>
 );
 
