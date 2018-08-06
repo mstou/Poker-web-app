329d4393211467476b4ae818435a4526f2f8aa82
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-07 01:49:00 +0300

    Reposition Ready button and add styles

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Buttons = ({winner,Ready,ChangeCards}) => (
-  <div>
-    {(!winner) ? <button className="ReadyButton" onClick={Ready}>Ready!</button> : undefined }
+  <div className="Buttons" >
+    {(!winner) ? <button className="ReadyButton" onClick={Ready}>Ready</button> : undefined }
     <p>{(winner) ? winner + " is the Winner!" : undefined }</p>
     {/*(!winner) ? <button onClick={ChangeCards}> Change Cards!</button> : undefined */}
   </div>
