a1b5d8661d9e2ed37895cb36701027eb31330668
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-06 19:09:01 +0300

    Place player's 1 hand below player's 2 hand in AllHands.js . Add poker table picture

--- src/components/AllHands/AllHands.js
+++ src/components/AllHands/AllHands.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { Hand } from './Hand';
 
 const AllHands = ({Cards,SelectedCards,onCardClick}) => (
-  <div>
-    <div>
-        <p> Player 1: </p>
-        <Hand cards={Cards[0].orderedCards} selectedCards={SelectedCards[0]} onClick={ (rank,suit) => onCardClick(rank,suit,0)}/>
-    </div>
-    <div>
-        <p> Player 2: </p>
-        <Hand cards={Cards[1].orderedCards} selectedCards={SelectedCards[1]} onClick={(rank,suit) => onCardClick(rank,suit,1)}/>
-   </div>
+  <div className="pokerTable">
+
+   <Hand
+      className="player2Cards"
+      cards={Cards[1].orderedCards}
+      selectedCards={SelectedCards[1]}
+      onClick={(rank,suit) => onCardClick(rank,suit,1)}
+   />
+
+   <Hand
+      className="player1Cards"
+      cards={Cards[0].orderedCards}
+      selectedCards={SelectedCards[0]}
+      onClick={ (rank,suit) => onCardClick(rank,suit,0)}
+   />
+
+   <img className="pokerTableImg" src="/styles/pokerTable.jpg" />
+   
   </div>
 );
 
