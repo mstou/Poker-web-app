c6634c931dd439989ee6bd0e7b7da2596c632205
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-07 01:32:08 +0300

    Add poker table image and place hands on the table

--- src/components/AllHands/AllHands.js
+++ src/components/AllHands/AllHands.js
@@ -4,22 +4,26 @@ import { Hand } from './Hand';
 const AllHands = ({Cards,SelectedCards,onCardClick}) => (
   <div className="pokerTable">
 
-   <Hand
-      className="player2Cards"
-      cards={Cards[1].orderedCards}
-      selectedCards={SelectedCards[1]}
-      onClick={(rank,suit) => onCardClick(rank,suit,1)}
-   />
+  <div className="player2cards">
+     <Hand
+        className="player2Cards"
+        cards={Cards[1].orderedCards}
+        selectedCards={SelectedCards[1]}
+        onClick={(rank,suit) => onCardClick(rank,suit,1)}
+     />
+   </div>
 
-   <Hand
-      className="player1Cards"
-      cards={Cards[0].orderedCards}
-      selectedCards={SelectedCards[0]}
-      onClick={ (rank,suit) => onCardClick(rank,suit,0)}
-   />
+   <div className="player1Cards">
+     <Hand
+        className="player1Cards"
+        cards={Cards[0].orderedCards}
+        selectedCards={SelectedCards[0]}
+        onClick={ (rank,suit) => onCardClick(rank,suit,0)}
+     />
+   </div>
 
-   <img className="pokerTableImg" src="/styles/pokerTable.jpg" />
-   
+   {/*<img alt="" className="pokerTableImg" src="/styles/pokerTable.jpg" />*/}
+
   </div>
 );
 
